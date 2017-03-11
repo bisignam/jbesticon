4f69bd4c744b113d6b38c8ee8b2c466ee2759154
Mario Bisignani <mario.bisignani@toodai.ch>
2017-03-11 18:41:30 +0100

    Test fetch of links from a page with a base tag
    An additional test has been added to FaviconsLinksFetcherTest class, the
    new test fetches links from a version of the newyorker.com page with a
    <base> tag in the <header> section.

--- src/test/java/ch/bisi/jbesticon/fetcher/FaviconsLinksFetcherTest.java
+++ src/test/java/ch/bisi/jbesticon/fetcher/FaviconsLinksFetcherTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-
 /**
  * Tests for {@link FaviconsLinksFetcher}.
  */
@@ -48,11 +47,33 @@ public class FaviconsLinksFetcherTest {
    * @throws IOException in case of problems reading the tests resources
    */
   @Test
+  public void getTagsForNewYorker_with_base_tag() throws IOException {
+    String newYorkerDomain = "http://www.newyorker.com";
+    Document newYorkerIndex = Jsoup.parse(
+        this.getClass().getResourceAsStream("/newyorker_with_base_tag" + ".html"), "UTF-8",
+        newYorkerDomain);
+    FaviconsLinksFetcher fetcher = new FaviconsLinksFetcher(newYorkerIndex);
+    List<URL> faviconsLinks = fetcher.getTagsUrls();
+    assertUrlsContainPaths(faviconsLinks, newYorkerDomain,
+        "images/wp-content/assets/dist/img/icon/apple-touch-icon-114x114-precomposed.png",
+        "images/wp-content/assets/dist/img/icon/apple-touch-icon-144x144-precomposed.png",
+        "images/wp-content/assets/dist/img/icon/apple-touch-icon-57x57-precomposed.png",
+        "images/wp-content/assets/dist/img/icon/apple-touch-icon-precomposed.png",
+        "images/wp-content/assets/dist/img/icon/apple-touch-icon.png",
+        "images/wp-content/assets/dist/img/icon/favicon.ico");
+  }
+
+  /**
+   * Tests getting favicons links from a well-formed html.
+   *
+   * @throws IOException in case of problems reading the tests resources
+   */
+  @Test
   public void getTagsForDaringFireball() throws IOException {
     String daringFireballDomain = "http://daringfireball.net/";
     Document newYorkerIndex = Jsoup
-                                .parse(this.getClass().getResourceAsStream("/daringfireball.html"),
-                                  "UTF-8", daringFireballDomain);
+        .parse(this.getClass().getResourceAsStream("/daringfireball.html"),
+            "UTF-8", daringFireballDomain);
     FaviconsLinksFetcher fetcher = new FaviconsLinksFetcher(newYorkerIndex);
     List<URL> faviconsLinks = fetcher.getTagsUrls();
     assertUrlsContainPaths(faviconsLinks, daringFireballDomain, "/graphics/apple-touch-icon.png",
@@ -78,7 +99,7 @@ public class FaviconsLinksFetcherTest {
    * Builds an {@link URL} given a domain and a path.
    *
    * @param domain the domain
-   * @param path   the path
+   * @param path the path
    */
   private URL buildUrl(final String domain, final String path) {
     try {
@@ -92,17 +113,17 @@ public class FaviconsLinksFetcherTest {
    * Utility method for asserting that a {@link List} contains the same elements of another one.
    * Note that the method doesn't care about the ordering of elements.
    *
-   * @param target   the {@link List} to check
-   * @param contains the {@link List} of elements that should be present in the target
-   *                 {@link List}.
+   * @param target the {@link List} to check
+   * @param contains the {@link List} of elements that should be present in the target {@link
+   * List}.
    */
   private <T> void assertContainSameElements(final List<T> target, final List<T> contains,
-                                              final Comparator<T> comparator) {
+      final Comparator<T> comparator) {
     assertEquals(contains.size(), target.size());
     target.sort(comparator);
     contains.sort(comparator);
     IntStream.range(0, target.size())
-      .forEachOrdered(i -> assertEquals(target.get(i), contains.get(i)));
+        .forEachOrdered(i -> assertEquals(contains.get(i), target.get(i)));
   }
 
 }
